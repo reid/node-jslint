f695efb592e6fd8fe72ffbb44607e6b0803cdece
Luke Smith <lsmith@yahoo-inc.com>
2010-03-31 17:14:26 -0700

    Update to use 0.1.33 compatible sys.exec API. Also wrapped the sys calls in a new callback queue method.

--- install.js
+++ install.js
@@ -5,10 +5,8 @@ var sys = require("sys"),
     dir = null;
 
 function cleanAndExit(ex) {
-    var code = 0,
-        exit = function () {
-            process.exit(code);
-        };
+    var code = 0;
+
     if (ex) {
         code = 1;
         if ("number" === typeof(ex)) {
@@ -18,28 +16,68 @@ function cleanAndExit(ex) {
             puts(ex.name + ": " + ex.message);
         }
     }
-    puts("Removing working directory");
-    if (dir) return sys.exec("rm -rf " + dir).addCallback(exit);
-    exit();
+    if (dir) {
+        puts("Removing working directory");
+        return sys.exec("rm -rf " + dir, function () {
+            process.exit( code );
+        });
+    } else {
+        process.exit(code);
+    }
 }
 
-function ex(cmd) {
-    puts("Running: " + cmd);
-    sys.exec(cmd).wait();
+function execCommands() {
+    var q = [].slice.call(arguments);
+
+    function run( error, out, err ) {
+        puts("run()");
+        if (error) {
+            puts("Error: " + err);
+            process.exit();
+        }
+
+        var command = q.shift();
+
+        switch (typeof command) {
+            case "string":
+                puts("Running " + command);
+                sys.exec(command, run);
+                break;
+            case "function":
+                try {
+                    out = command(out);
+                } catch (e) {
+                    error = true;
+                    err = out = e.message;
+                }
+
+                run( error, out, err );
+                break;
+        }
+    }
+
+    run();
 }
 
 process.addListener("uncaughtException", cleanAndExit);
 
-dir = sys.exec("mktemp -d /tmp/jslint.XXXX").wait().shift().replace(/\n/, "");
-process.chdir(dir);
-puts("Changed to new working directory " + dir);
-
-ex("git init");
-ex("git remote add origin git://github.com/reid/node-jslint.git");
-ex("git pull origin master");
-ex("git submodule update --init");
-ex("cp -Rf bin lib /usr/local");
-
-puts("Installed jslint into /usr/local");
-
-cleanAndExit();
+execCommands(
+    "mktemp -d /tmp/jslint.XXXX",
+    function ( out ) {
+        dir = out.replace(/\n/,'');
+        return dir;
+    },
+    function ( dir ) {
+        process.chdir( dir );
+        puts("Changed to new working directory " + dir);
+        return dir;
+    },
+    "git init",
+    "git remote add origin git://github.com/reid/node-jslint.git",
+    "git pull origin master",
+    "git submodule update --init",
+    "cp -Rf bin lib /usr/local",
+    function () {
+        puts("Installed jslint into /usr/local");
+        cleanAndExit();
+    });
