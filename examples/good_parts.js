12efc5e3c5bcdbf582d2357e25593449b2ed5cc9
Fabricio C Zuardi <fabricio@fabricio.org>
2010-09-21 16:28:50 -0300

    two examples of auxiliary files to be concatenated before passing the output to node-jslint

---
+++ examples/good_parts.js
@@ -0,0 +1,1 @@
+/*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
---
+++ examples/node_globals.js
@@ -0,0 +1,1 @@
+/*globals require, process */
