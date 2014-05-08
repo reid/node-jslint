548c9ca91bff24b29fcf5d4b261a0e14a72d0ce2
Sam Mikes <smikes@cubane.com>
2014-05-08 17:15:47 +0100

    test: add stream tests

---
+++ test/reportstream.js
@@ -0,0 +1,79 @@
+var assert = require('assert'),
+    stream = require('stream'),
+    ReportStream = require('../lib/reportstream.js'),
+    JSONReportStream = require('../lib/jsonreportstream.js');
+
+suite('reportstream', function () {
+    test('can create object', function () {
+        var r = new ReportStream();
+
+        assert.ok(r instanceof ReportStream);
+        assert.ok(r instanceof stream.Transform);
+    });
+
+    test('can async', function (done) {
+        var r = new ReportStream();
+
+        r.on('data', function(chunk) {
+            assert.deepEqual(chunk, '\nexample.js is OK.');
+            done();
+        });
+
+        r.write({file: 'example.js', linted: {ok: true}});
+        r.end();
+    });
+
+    test('can make a colorized reporter', function (done) {
+        var r = new ReportStream({color: true});
+
+        r.on('data', function(chunk) {
+            assert.deepEqual(chunk, '\n\x1b[1mexample.js\x1b[0m is \x1b[32mOK\x1b[0m.');
+            done();
+        });
+
+        r.write({file: 'example.js', linted: {ok: true}});
+        r.end();
+    });
+
+    test('can make a terse reporter', function (done) {
+        var r = new ReportStream({terse: true});
+
+        r.on('data', function(chunk) {
+            assert.deepEqual(chunk, '.');
+            done();
+        });
+
+        r.write({file: 'example.js', linted: {ok: true}});
+        r.end();
+    });
+
+    test('can make two reporters with diff. behavior', function (done) {
+        var r1 = new ReportStream(),
+            r2 = new ReportStream({terse: true});
+
+        r1.on('data', function (chunk) {
+            r2.write({file: 'example.js', linted: {ok: true}});
+            r2.end();
+        });
+        r2.on('data', function (chunk) {
+            assert.equal(chunk, '.');
+            done();
+        });
+
+        r1.write({file: 'example.js', linted: {ok: true}});
+        r1.end();
+    });
+
+    test('how about a JSONReportStream', function (done) {
+        var r = new JSONReportStream();
+
+        r.on('data', function (chunk) {
+            assert.deepEqual(chunk, JSON.stringify(['example.js', null]));
+            done();
+        });
+
+        r.write({file: 'example.js', linted: {ok: true}});
+        r.end();
+    });
+
+});
