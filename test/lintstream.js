0b04a6fecb1cdb0d5aca3795b47c9d36fda52732
Sam Mikes <smikes@cubane.com>
2014-05-08 21:04:07 +0100

    test: new test for LintStream

---
+++ test/lintstream.js
@@ -0,0 +1,39 @@
+// this file is test/lintstream.js
+// tests for stream interface to JSLint
+//
+// Copyright 2014 Cubane Canada Inc.
+//
+// Released under modified MIT/BSD 3-clause license
+// See LICENSE for details.
+
+var assert = require('assert'),
+    stream = require('stream'),
+    LintStream = require('../lib/lintstream.js');
+
+suite('lintstream', function () {
+    test('can create object', function () {
+        var l = new LintStream();
+
+        assert.ok(l instanceof LintStream);
+        assert.ok(l instanceof stream.Transform);
+    });
+
+    test('can create object incorrectly', function () {
+        var l = LintStream();
+
+        assert.ok(l instanceof LintStream);
+        assert.ok(l instanceof stream.Transform);
+    });
+
+    test('can lint a file', function (done) {
+        var l = new LintStream();
+
+        l.on('data', function (chunk) {
+            assert.equal(chunk.file, 'example.js');
+            assert.deepEqual(chunk.linted, {ok: true, errors: [], options: {node: true, es5: true}});
+            done();
+        });
+
+        l.write({file: 'example.js', body: ''});
+    });
+});
