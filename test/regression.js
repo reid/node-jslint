abdeb59c27b0a0ab4448d95c7f25521e97682bd7
Sam Mikes <smikes@cubane.com>
2014-10-22 22:22:41 +0100

    add test file for misc github cases

---
+++ test/regression.js
@@ -0,0 +1,27 @@
+var assert = require('assert'),
+    nodelint = require('../lib/nodelint'),
+    linter = require('../lib/linter');
+
+suite('case #101', function () {
+    // https://github.com/reid/node-jslint/issues/101
+    test('has warning with default options', function () {
+
+        var options = {edition: 'latest'},
+            JSLINT = nodelint.load(options.edition),
+            script = "console.log('a');\n",
+            result = linter.doLint(JSLINT, script, options);
+
+        assert.ok(!result.ok);
+    });
+
+    test('no warning with node option', function () {
+
+        var options = {edition: 'latest', node: true},
+            JSLINT = nodelint.load(options.edition),
+            script = "console.log('a');\n",
+            result = linter.doLint(JSLINT, script, options);
+
+        assert.ok(result.ok);
+    });
+
+});
