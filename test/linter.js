b8cdd6ffe18e39036c0c0f73263af919afb07098
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2014-10-26 01:44:55 -0700

    Remove old traces of the edition option.

--- test/linter.js
+++ test/linter.js
@@ -4,70 +4,50 @@ var assert = require('assert'),
     linter = require('../lib/linter');
 
 suite('preprocessScript', function () {
+
     test('removes leading BOM', function () {
-
         assert.equal('var x=1;', linter.preprocessScript('var x=1;'));
-
         assert.equal('var x=1;', linter.preprocessScript('\uFEFFvar x=1;'));
-
     });
+
     test('removes shebang', function () {
-
         assert.equal('\nvar x=1;', linter.preprocessScript('#!/usr/bin/env node\nvar x=1;'));
-
     });
+
 });
 
 suite('lint', function () {
     var oldHome = process.env.HOME;
+
+    setup(function () {
+        // Don't let user's config interfere with the test.
+        process.env.HOME = '';
+    });
+
     teardown(function () {
         process.env.HOME = oldHome;
     });
 
     test('basic lint step', function () {
-
-        var script = "// only a comment\n",
-            options = {edition: 'latest'},
-            result;
-
-        // don't let user's config interfere with our test
-        process.env.HOME = '';
-
-        result = linter.doLint(script, options);
-
+        var result = linter.doLint('// only a comment\n');
         assert.ok(result.ok);
         assert.deepEqual(result.errors, []);
     });
 
     test('lint finds error', function () {
-
-        var script = "//TODO: remove this\n",
-            options = {edition: '2013-09-22'},
-            result;
-
-        // don't let user's config interfere with our test
-        process.env.HOME = '';
-
-        result = linter.doLint(script, options);
-
+        var result = linter.doLint('//TODO: remove this\n');
         assert.strictEqual(1, result.errors.length);
-        assert.strictEqual("Unexpected TODO comment.",
-                           result.errors[0].raw);
+        assert.strictEqual('Unexpected TODO comment.', result.errors[0].raw);
 
     });
 
     test('maxerr causes null error', function () {
-        var script = "var __evil = eval('3')",
-            options = {maxerr: 1},
-            result;
-
-        result = linter.doLint(script, options);
-
+        var result = linter.doLint('var __evil = eval(\'3\')', {
+            maxerr: 1
+        });
         assert.equal(result.ok, false);
         assert.equal(result.errors.length, 3);
         assert.equal(result.errors[2], null);
-
     });
-
 
 });
--- test/regression.js
+++ test/regression.js
@@ -4,22 +4,17 @@ var assert = require('assert'),
     linter = require('../lib/linter');
 
 suite('case #101', function () {
+
     // https://github.com/reid/node-jslint/issues/101
     test('has warning with default options', function () {
-
-        var options = {edition: 'latest'},
-            script = "console.log('a');\n",
-            result = linter.doLint(script, options);
-
+        var result = linter.doLint('console.log(\'a\');\n');
         assert.ok(!result.ok);
     });
 
     test('no warning with node option', function () {
-
-        var options = {edition: 'latest', node: true},
-            script = "console.log('a');\n",
-            result = linter.doLint(script, options);
-
+        var result = linter.doLint('console.log(\'a\');\n', {
+            node: true
+        });
         assert.ok(result.ok);
     });
 
